529ca5d19c37574377e042a95f8357f77beebf69
kimkwokwong <kimkwokwong@gmail.com>
2016-09-06 11:10:54 +0800

    Initial Commit

---
+++ Flight_list/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flight_list
+//
+//  Created by Wong kim kwok on 6/9/2016.
+//  Copyright © 2016 CX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Flight_listTests/Flight_listTests.swift
@@ -0,0 +1,36 @@
+//
+//  Flight_listTests.swift
+//  Flight_listTests
+//
+//  Created by Wong kim kwok on 6/9/2016.
+//  Copyright © 2016 CX. All rights reserved.
+//
+
+import XCTest
+@testable import Flight_list
+
+class Flight_listTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
